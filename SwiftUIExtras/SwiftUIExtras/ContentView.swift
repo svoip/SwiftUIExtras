861f76e7541776ec61de80aef8107479d3420018
Sardorbek <svoip.mobile@gmail.com>
2026-02-04 16:20:30 +0100

    Add xcode project

---
+++ SwiftUIExtras/SwiftUIExtras/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIExtras
+//
+//  Created by Sardorbek Ruzmatov on 04/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIExtras/SwiftUIExtras/SwiftUIExtrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExtrasApp.swift
+//  SwiftUIExtras
+//
+//  Created by Sardorbek Ruzmatov on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExtrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
